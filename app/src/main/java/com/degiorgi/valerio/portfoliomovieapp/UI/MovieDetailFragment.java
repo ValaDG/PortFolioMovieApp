e20634cfe0ebe7117c5aaf867c444ad431f80220
valerio <valakias.luceoscura@gmail.com>
2016-02-29 12:50:01 +0100

    Added reviews from network call

--- app/src/main/java/com/degiorgi/valerio/portfoliomovieapp/UI/MovieDetailFragment.java
+++ app/src/main/java/com/degiorgi/valerio/portfoliomovieapp/UI/MovieDetailFragment.java
@@ -6,11 +6,26 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import com.degiorgi.valerio.portfoliomovieapp.MovieService;
 import com.degiorgi.valerio.portfoliomovieapp.R;
+import com.degiorgi.valerio.portfoliomovieapp.models.MovieReviewsForId;
+import com.degiorgi.valerio.portfoliomovieapp.models.MovieTrailersForId;
+import com.degiorgi.valerio.portfoliomovieapp.models.SingleReviewResult;
 import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
  * Created by Valerio on 28/02/2016.
@@ -19,18 +34,22 @@ public class MovieDetailFragment extends Fragment
 
 {
 
+    public static final String API_BASE_URL = "http://api.themoviedb.org/";
+    String api_key = "241141bc665e9b2d0fb9ac4759497786";
+    ArrayAdapter<String> mReviewsAdapter;
+
+    String url;
+    String title;
+    String release;
+    String synopsis;
+    double userRating;
+    int id;
 
     //Single detail activity for single movies, we receive the intents from the movie fragment and update each view with the correct values
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        String url;
-        String title;
-        String release;
-        String synopsis;
-        double userRating;
-        int id;
 
         View rootview = inflater.inflate(R.layout.movie_detail_fragment, container, false);
 
@@ -60,8 +79,63 @@ public class MovieDetailFragment extends Fragment
             OverView.setText(synopsis);
         }
 
+        mReviewsAdapter = new ArrayAdapter<String>(getActivity(),
+                R.layout.single_list_item_textview,
+                R.id.single_item_textview_id,
+                new ArrayList<String>());
+
+        getReviews(id);
+
+        ListView reviewsListView = (ListView) rootview.findViewById(R.id.reviews_listview);
+        reviewsListView.setAdapter(mReviewsAdapter);
 
         return rootview;
+    }
+
+
+    public ArrayList<String> getTrailers(int id){
+
+        Retrofit retrofit = new Retrofit.Builder().baseUrl(API_BASE_URL).addConverterFactory(GsonConverterFactory.create()).build();
+
+        MovieService.FetchMovieInterface MovieInterface = retrofit.create(MovieService.FetchMovieInterface.class);
+
+        Call<MovieTrailersForId> callMovies = MovieInterface.getMovieTrailers(id,api_key);
+
+        return new ArrayList<>();
+    }
+
+    public void getReviews(int id){
+
+        Retrofit retrofit = new Retrofit.Builder().baseUrl(API_BASE_URL).addConverterFactory(GsonConverterFactory.create()).build();
+
+        MovieService.FetchMovieInterface MovieInterface = retrofit.create(MovieService.FetchMovieInterface.class);
+
+        Call<MovieReviewsForId> callReviews = MovieInterface.getMovieReviews(id,api_key);
+
+        callReviews.enqueue(new Callback<MovieReviewsForId>() {
+            @Override
+            public void onResponse(Call<MovieReviewsForId> call, Response<MovieReviewsForId> response) {
+
+                List<SingleReviewResult> singleList = response.body().getResults();
+
+
+
+                for(SingleReviewResult result : singleList)
+                {
+                    mReviewsAdapter.add(result.getContent());
+
+                }
+
+
+
+            }
+
+            @Override
+            public void onFailure(Call<MovieReviewsForId> call, Throwable t) {
+
+            }
+        });
+
     }
 
 }
